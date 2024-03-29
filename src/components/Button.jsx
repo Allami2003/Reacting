e07a14ffcc320c8ced561b5afe06625001839c74
Allami2003 <130898259+Allami2003@users.noreply.github.com>
2024-03-29 13:29:25 +0700

    Add files via upload

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Button = ({label, onclick}) =>{
-    return <button onClick={onclick}>{label}</button>
-}
+    /*return <button onClick={onclick}>{label}</button>*/
+    const [count,setCount]=useState(0);
+    return(
+      <div>
+        <p>вы нажили {count}</p>
+        <button onClick={()=>setCount(count+1)}>mimi</button>
+      </div>
+    );
+  }
 export default Button
